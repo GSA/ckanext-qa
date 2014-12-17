86457713c78977bb660f20d813e5b9082ccac767
yaditi <ydave@reisys.com>
2014-12-17 11:35:38 -0500

    Cron optimization

--- ckanext/qa/commands.py
+++ ckanext/qa/commands.py
@@ -6,6 +6,7 @@ import logging
 from pylons import config
 from urllib2 import Request, urlopen, URLError, HTTPError
 import math
+import os.path
 
 import ckan.plugins as p
 
@@ -55,43 +56,50 @@ class QACommand(p.toolkit.CkanCommand):
         # won't get disabled
         self.log = logging.getLogger('ckanext.qa')
 
-        if cmd == 'update':            
+        if cmd == 'update':
             self.update_resource_rating()
-                
-        elif cmd == 'update_sel': 
+
+        elif cmd == 'update_sel':
             from ckan import model
-            sql = '''select last_updated::date from task_status order by last_updated desc limit 1;'''
-            q = model.Session.execute(sql)            
+            #sql = '''select last_updated::date from task_status order by last_updated desc limit 1;'''
+            '''q = model.Session.execute(sql)
             for row in q:
-               last_updated = str(row['last_updated']) + 'T00:00:000Z'
-            
-            url = config.get('solr_url') + "/select?q=metadata_modified:[" + last_updated + "%20TO%20NOW]&wt=json&indent=true&fl=name"
+               last_updated = str(row['last_updated']) + 'T00:00:000Z'  '''
+
+            if os.path.isfile('/var/log/ckan_qa_date_log.txt') and os.stat("/var/log/ckan_qa_date_log.txt").st_size > 1:
+                file = open('/var/log/ckan_qa_date_log.txt', 'r')
+                last_updated = file.readline() + 'Z'
+            else:
+                last_updated = '2012-01-01T00:00:000Z'
+
+            print last_updated
+            url = config.get('solr_url') + "/select?q=metadata_modified:[" + last_updated + "%20TO%20NOW]&sort=metadata_modified+asc&wt=json&indent=true&fl=name"
+
             response = self.get_data(url)
-            
+
             if (response != 'error'):
                 f = response.read()
                 data = json.loads(f)
                 rows = data.get('response').get('numFound')
 
                 start = 0
-                chunk_size = 1000        
-          
+                chunk_size = 1000
+
                 for x in range(0, int(math.ceil(rows/chunk_size))+1):
-		  
+
                     if(x == 0):
                         start = 0
-                    
+
                     response = self.get_data(url + "&rows=" + str(chunk_size) + "&start=" + str(start))
                     f = response.read()
                     data = json.loads(f)
                     results = data.get('response').get('docs')
-                
+
                     for x in range(0, len(results)):
-                        #self.log.info(results[x]['name'])
-                        self.args.append(results[x]['name'])            
-                        self.update_resource_rating() 
-                        self.args.pop()            
-             
+                        self.args.append(results[x]['name'])
+                        self.update_resource_rating()
+                        self.args.pop()
+
         elif cmd == 'clean':
             self.log.error('Command "%s" not implemented' % (cmd,))
 
@@ -111,10 +119,10 @@ class QACommand(p.toolkit.CkanCommand):
           print 'Reason: ', e.reason
           return 'error'
         else:
-          return response        
-                
-    def update_resource_rating(self):               
-        
+          return response
+
+    def update_resource_rating(self):
+
         from ckan import model
         from ckan.model.types import make_uuid
 
@@ -130,9 +138,9 @@ class QACommand(p.toolkit.CkanCommand):
             'apikey': user.get('apikey'),
             'username': user.get('name'),
         })
-        
+
         for package in self._package_list():
-            self.log.info("QA on dataset being added to Celery queue: %s (%d resources)" % 
+            self.log.info("QA on dataset being added to Celery queue: %s (%d resources)" %
                                 (package.get('name'), len(package.get('resources', []))))
 
             for resource in package.get('resources', []):
@@ -140,7 +148,7 @@ class QACommand(p.toolkit.CkanCommand):
                 pkg = model.Package.get(package['id'])
                 if pkg:
                   resource['is_open'] = pkg.isopen()
-                  data = json.dumps(resource) 
+                  data = json.dumps(resource)
                   task_id = make_uuid()
                   task_status = {
                       'entity_id': resource['id'],
@@ -150,16 +158,15 @@ class QACommand(p.toolkit.CkanCommand):
                       'value': task_id,
                       'error': u'',
                       'last_updated': datetime.datetime.now().isoformat()
-                  }  
+                  }
                   task_context = {
                       'model': model,
                       'user': user.get('name')
-                  } 
+                  }
 
                   p.toolkit.get_action('task_status_update')(task_context, task_status)
-                  #tasks.update.apply_async(args=[context, data], task_id=task_id)
                   tasks.update(context, data)
-                
+
     def make_post(self, url, data):
             headers = {'Content-type': 'application/json',
                        'Accept': 'text/plain'}
@@ -168,7 +175,7 @@ class QACommand(p.toolkit.CkanCommand):
     def get_response(self, url, data):
         response = json.loads(requests.get(url, params=data).text)
         return response
-    
+
     def _package_list(self):
         """
         Generate the package dicts as declared in self.args.
@@ -183,40 +190,39 @@ class QACommand(p.toolkit.CkanCommand):
         if len(self.args) > 1:
             for id in self.args[1:]:
                 data = {'id': unicode(id)}
-                url = api_url + '/package_show'     
+                url = api_url + '/package_show'
 
-                response = self.get_response(url, data)                         
-                
+                response = self.get_response(url, data)
                 if not response.get('success'):
                     err = ('Failed to get package %s from url %r: %s' %
                            (id, url, response.get('error')))
                     self.log.error(err)
-                    #raise CkanApiError(err)
+                    return
+
+                fo = open("/var/log/ckan_qa_date_log.txt", "wb")
+                fo.write( response.get('result').get('metadata_modified'));
+                fo.close()
+
                 yield response.get('result')
         else:
-            #page, limit = 1, 100
             page, limit = 0, 100
             url = api_url + '/current_package_list_with_resources'
-            #response = self.make_post(url, {'page': page, 'limit': limit})
             response = self.get_response(url, {'start': page, 'rows': limit})
-                        
+
             if not response.get('success'):
                 err = ('Failed to get package list with resources from url %r: %s' %
                        (url, response.get('error')))
                 self.log.error(err)
-                #raise CkanApiError(err)
             chunk = response.get('result').get('results')
             while(chunk):
-                #page += 1
                 page = page + limit
                 for p in chunk:
+                    print p
                     yield p
                 url = api_url + '/current_package_list_with_resources'
-                #response = self.make_post(url, {'page': page, 'limit': limit})
                 response = self.get_response(url, {'start': page, 'rows': limit})
 
                 try:
-                    #response.raise_for_status()
                     data = {'start': page, 'rows': limit}
                     r = requests.get(url, params=data)
                     r.raise_for_status()
@@ -224,6 +230,6 @@ class QACommand(p.toolkit.CkanCommand):
                     err = ('Failed to get package list with resources from url %r: %s' %
                        (url, str(e)))
                     self.log.error(err)
-                    #raise CkanApiError(err)
+                    continue
+
                 chunk = response.get('result').get('results')
-                
